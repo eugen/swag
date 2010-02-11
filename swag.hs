3162466b7e6ceea2c60bc99feb15da1845e022f0
c15c8ra1n <eugen.anghel@softhouse-int.com>
2010-02-11 18:45:19 +0200

    changes

--- swag.hs
+++ swag.hs
@@ -1,20 +1,22 @@
 module Main
 where
 import Prelude
-import System.Environment
-import System.Directory
-import System.FilePath 
-import System.Process (readProcess, readProcessWithExitCode)
 import Data.List
 import Data.Maybe
 import Data.Time
 import Data.Time.Clock
-import System.Locale
-import Text.StringTemplate
 import Control.Monad
+import Text.StringTemplate
 import Text.Pandoc
 import Data.Bool.Higher
 import System
+import System.IO.Unsafe
+import System.Locale
+import System.Environment
+import System.Directory
+import System.FilePath 
+import System.Process (readProcess, readProcessWithExitCode)
+
 
 (~>) = flip ($)
 infixl 1 ~>
@@ -28,16 +30,18 @@ partitionM :: (Monad m) => (a -> m Bool) -> [a] -> m ([a], [a])
 partitionM f xs = foldM (\ (l,r) e -> return . ((e:l,r) ?? (l,e:r)) =<< f e) ([],[]) $ reverse xs
 
 data Page = Page {
-      fileName :: String, 
-      htmlContent :: String, 
-      published :: UTCTime 
+      pageFile :: String, 
+      pageTitle :: String,
+      pageContent :: String, 
+      pagePublished :: UTCTime 
     }
 
 data PageDir = PageDir {
+      dirPath :: String,
       dirName :: String,
-      pages :: [Page], 
-      templates :: STGroup String,
-      children :: [PageDir] 
+      dirPages :: [Page], 
+      dirTemplates :: STGroup String,
+      dirChildren :: [PageDir] 
     }
 
 pageFiles files = 
@@ -51,12 +55,16 @@ commitDate path = do
   setCurrentDirectory oldcd
   maybe getCurrentTime return (parseTime defaultTimeLocale "%s" dateStr)
 
+getPageTitle content = 
+    if take 2 title == "% " then drop 2 title else "Untitled"
+        where title = head $ lines content
+            
 loadPage :: String -> IO Page
 loadPage path = do
-  content <- readFile path >>= return . writeHtmlString defaultWriterOptions . readMarkdown defaultParserState
+  content <- readFile path
   published <- commitDate path
   fileName <- return (dropExtension $ takeFileName path)
-  return $ Page fileName content published
+  return $ Page fileName (getPageTitle content) ((writeHtmlString defaultWriterOptions . readMarkdown defaultParserState) content) published
 
 loadContent :: String -> STGroup String -> IO PageDir 
 loadContent path parentTemplates = do
@@ -66,7 +74,7 @@ loadContent path parentTemplates = do
   ownTemplates <- doesDirectoryExist (path </> "templates") >>= directoryGroup (path </> "templates") ?? return nullGroup
   templates <- return $ mergeSTGroups ownTemplates parentTemplates
   children <- mapM (flip loadContent templates) (map (path </>) subDirs)
-  return (PageDir dirName pages templates children)
+  return (PageDir path dirName pages templates children)
 
 buildSite :: String -> IO ()
 buildSite dir = do
@@ -74,36 +82,24 @@ buildSite dir = do
   buildDir "/" content
   return ()
 
+buildPage templates attributes page@(Page file title content published) =
+    let
+        templates' = maybe templates
+                     (\ t -> mergeSTGroups (groupStringTemplates [("content", t)]) templates)
+                     (getStringTemplate' file templates)
+        template = fromJust (getStringTemplate "index" templates')
+        attributes' = attributes ++ [("content", [content]), ("title", [title]), ("published", [show published])]
+        templateAttr = setManyAttrib attributes' template 
+    in
+       render templateAttr
+
 buildDir :: String -> PageDir -> IO ()
-buildDir relPath (PageDir dirName pages templates children) =
+buildDir relPath (PageDir path dir pages templates children) =
     let attributes = [
          ("relPath", [relPath]),
          ("children", [dirName c | c <- children]),
-         ("pages", map fileName pages)]
-    in
-      putStrLn attributes
+         ("pages", map pageFile pages)]
+    in do
+      mapM_ (\ p@(Page {pageFile=file}) -> writeFile (path </> file ++ ".html") (buildPage templates attributes p)) pages
+      mapM_ (\c -> buildDir (relPath ++ dirName c ++ "/") c) children
          
-
--- buildDir :: STGroup String -> String -> IO ()
--- buildDir parentTemplates dir = do
---   hasOwnTemplates <- doesDirectoryExist (dir </> "templates")
---   templates <- if hasOwnTemplates 
---                then liftM (flip mergeSTGroups parentTemplates) (directoryGroup $ dir </> "templates") 
---                else return parentTemplates
---   (subdirs,files) <- getDirectoryContentsEx dir
---   children <- mapM (\ sd -> getDirectoryContentsEx (dir </> sd) >>= (\ cs -> return $ (sd, map dropExtension $ goodFiles $ snd cs))) subdirs
---   mapM (\ file -> buildFile templates (dir </> file) children) (goodFiles files)
---   mapM (\ subdir -> buildDir templates (dir </> subdir)) subdirs
---   return ()
-
--- buildFile :: STGroup String -> String -> [(String, [String])]-> IO ()
--- buildFile templates file attribs = do
---     fileContent <- readFile file
---     template <- return $ fromJust $ getStringTemplate "main" templates
---     htmlFragment <- return $ writeHtmlString defaultWriterOptions $ readMarkdown defaultParserState fileContent
---     template' <- return $ setAttribute "content" htmlFragment template
---     putStrLn (show attribs)
---     template'' <- return $ setManyAttrib attribs template' --foldl (flip setManyAttrib) template' attribs
---     html <- return $ render $ template''
---     writeFile (replaceExtension file ".html") html
-
